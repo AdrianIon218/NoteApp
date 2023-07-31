a2f6df0a531b1533d3fa1a73119a8de8f26c3015
AdrianIon218 <ionadrian218@yahoo.com>
2023-07-31 23:33:38 +0300

    change the no page appearcence

--- src/components/Pages/NoPage.tsx
+++ src/components/Pages/NoPage.tsx
@@ -3,12 +3,12 @@ function NoPage() {
   return (
     <section className="no-page-ctn">
      <div className="no-page">
-      <h1>
-        Sorry, the page you accessed does not exist <span className="red">!</span>
-      </h1>
       <p className="emoji">
          &#128533;
       </p>
+      <h1>
+        Sorry, the page you accessed does not exist <span className="red">!</span>
+      </h1>
      </div>
     </section>
   )
