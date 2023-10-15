338ec251292a2aa06d5e4c43432f73a029da1c5e
AdrianIon218 <ionadrian218@yahoo.com>
2023-10-15 14:05:07 +0300

    styling the logo in thr footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="footer">
-      <img src="./note-logo-footer.png" alt="Note" className="footer__logo" />
+      <div className="footer__logo" />
       <div className="footer__logo_name">Notes</div>
     </footer>
   );
