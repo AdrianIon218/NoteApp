d28641664b687eef058eed431d00dd9a81c5b686
AdrianIon218 <ionadrian218@yahoo.com>
2023-10-21 16:21:13 +0300

    deactivate hover effect for mobile device

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,24 +2,19 @@ import { useState } from "react";
 
 export default function Footer() {
   const [footerPointerStyle, setFooterStyle] = useState({});
-  const [isMobile, setIsMobile] = useState(false);
-  const isFirstEnterOfUser = Object.keys(footerPointerStyle).length === 0;
 
   function mouseHandler(e: any) {
-    if (!isMobile) {
-      let rect = e.target.getBoundingClientRect();
-      let x = e.clientX - rect.left;
-      let y = e.clientY - rect.top;
-      setFooterStyle({ "--x": `${x}px`, "--y": `${y}px` });
-    }
+    let rect = e.target.getBoundingClientRect();
+    let x = e.clientX - rect.left;
+    let y = e.clientY - rect.top;
+    setFooterStyle({ "--x": `${x}px`, "--y": `${y}px` });
   }
 
   return (
     <footer
       className="footer"
       onMouseMove={mouseHandler}
-      onTouchStart={() => setIsMobile(true)}
-      style={isFirstEnterOfUser ? {} : footerPointerStyle}
+      style={footerPointerStyle}
     >
       <div className="footer__logo" />
       <div className="footer__logo_name">Notes</div>
