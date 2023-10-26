4ddbe470f7e9435712a3e28948121dcfed73b186
AdrianIon218 <ionadrian218@yahoo.com>
2023-10-26 22:15:13 +0300

    Updates

---
+++ assets/TextArea.e6fd1aae.js
@@ -0,0 +1,1 @@
+import{r as t,j as a}from"./index.da4ba273.js";function l(e,r){return a("div",{className:"field",children:a("textarea",{className:"textArea",placeholder:"Write here ...",autoComplete:"off",name:e.name,ref:r,defaultValue:e.value})})}const s=t.exports.forwardRef(l);export{s as T};
