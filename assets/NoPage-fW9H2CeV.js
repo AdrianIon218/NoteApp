05c4ad9bf5a8c18c080740767fa2e5ea825a83ec
AdrianIon218 <ionadrian218@yahoo.com>
2025-04-13 15:26:23 +0300

    Updates

--- assets/NoPage-fW9H2CeV.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-Cn_mJQYN.js";function a(){return e.jsx("section",{className:"no-page-ctn",children:e.jsxs("div",{className:"no-page",children:[e.jsx("p",{className:"emoji",children:"😕"}),e.jsxs("h1",{children:["Sorry, the page you accessed does not exist"," ",e.jsx("span",{className:"red",children:"!"})]})]})})}export{a as default};
